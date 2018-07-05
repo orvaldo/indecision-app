5958ba13745774b1898a6c53a832e2bfe3e883d0
Simon Orvarsson <simon.orvarsson@gmail.com>
2018-07-05 15:57:35 +0200

    styled components intro

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledTitle = styled.h1`
+    font-size: 3.2rem;
+    margin: 0;
+`;
+
+const StyledHeader = styled.div`
+    background: #20222b;
+    color: white;
+    margin-bottom: 4.8rem;
+    padding: 1.6rem 0;
+`;
+
+const StyledSubtitle = styled.h2`
+    color: #a5afd7;
+    font-size: 1.6rem;
+    font-weight: 500;
+`;
 
 const Header = (props) => (
-    <div>
-        <h1>{props.title}</h1>
-        {props.subtitle && <h2>{props.subtitle}</h2>}
-    </div>
+    <StyledHeader>
+        <StyledTitle>{props.title}</StyledTitle>
+        {props.subtitle && <StyledSubtitle>{props.subtitle}</StyledSubtitle>}
+    </StyledHeader>
 );
 
 Header.defaultProps = {
